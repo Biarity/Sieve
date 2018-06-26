02e4fb55b4a82de4bcca1cca3ba8988137200e2a
Biarity <biarity@outlook.com>
2018-06-26 14:59:33 +1000

    Model data contract

--- Sieve/Models/SieveModel.cs
+++ Sieve/Models/SieveModel.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace Sieve.Models
 {
+    [DataContract]
     public class SieveModel : ISieveModel<IFilterTerm, ISortTerm>
     {
+        [DataMember]
         public string Filters { get; set; }
 
+        [DataMember]
         public string Sorts { get; set; }
 
-        [Range(1, int.MaxValue)]
+        [DataMember, Range(1, int.MaxValue)]
         public int? Page { get; set; }
 
-        [Range(1, int.MaxValue)]
+        [DataMember, Range(1, int.MaxValue)]
         public int? PageSize { get; set; }
 
         public List<IFilterTerm> FiltersParsed
