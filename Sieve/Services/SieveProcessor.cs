95894027d21e60243a5c51ff478c86c5b8aa69d0
Alica Kacengova <alica.kacengova@gmail.com>
2021-01-23 22:25:12 +0100

    make GetClosureOverConstant really work

--- Sieve/Services/SieveProcessor.cs
+++ Sieve/Services/SieveProcessor.cs
@@ -316,12 +316,10 @@ namespace Sieve.Services
         }
 
         // Workaround to ensure that the filter value gets passed as a parameter in generated SQL from EF Core
-        // See https://github.com/aspnet/EntityFrameworkCore/issues/3361
-        // Expression.Constant passed the target type to allow Nullable comparison
-        // See http://bradwilson.typepad.com/blog/2008/07/creating-nullab.html
         private Expression GetClosureOverConstant<T>(T constant, Type targetType)
         {
-            return Expression.Constant(constant, targetType);
+            Expression<Func<T>> hoistedConstant = () => constant;
+            return Expression.Convert(hoistedConstant.Body, targetType);
         }
 
         private IQueryable<TEntity> ApplySorting<TEntity>(
