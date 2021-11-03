fd25649331e1d976f7d8c6bc0422b758e9d2fb32
Steven Decoodt <steven.decoodt@vinci-energies.net>
2021-11-03 16:53:47 +0100

    Update Readme, unit tests and sample

--- SieveUnitTests/Mapper.cs
+++ SieveUnitTests/Mapper.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Sieve.Exceptions;
 using Sieve.Models;
+using Sieve.Services;
 using SieveUnitTests.Entities;
 using SieveUnitTests.Services;
 using Xunit;
@@ -10,15 +11,10 @@ namespace SieveUnitTests
 {
     public class Mapper
     {
-        private readonly ApplicationSieveProcessor _processor;
         private readonly IQueryable<Post> _posts;
 
         public Mapper()
         {
-            _processor = new ApplicationSieveProcessor(new SieveOptionsAccessor(),
-                new SieveCustomSortMethods(),
-                new SieveCustomFilterMethods());
-
             _posts = new List<Post>
             {
                 new Post
@@ -45,23 +41,49 @@ namespace SieveUnitTests
             }.AsQueryable();
         }
 
-        [Fact]
-        public void MapperWorks()
+        /// <summary>
+        /// Processors with the same mappings but configured via a different method.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<object[]> GetProcessors()
+        {
+            yield return new object[] { 
+                new ApplicationSieveProcessor(
+                    new SieveOptionsAccessor(),
+                    new SieveCustomSortMethods(),
+                    new SieveCustomFilterMethods())};
+            yield return new object[] { 
+                new ModularConfigurationSieveProcessor(
+                    new SieveOptionsAccessor(),
+                    new SieveCustomSortMethods(),
+                    new SieveCustomFilterMethods())};
+            yield return new object[] { 
+                new ModularConfigurationWithScanSieveProcessor(
+                    new SieveOptionsAccessor(),
+                    new SieveCustomSortMethods(),
+                    new SieveCustomFilterMethods())};
+        }
+        
+        
+        [Theory]
+        [MemberData(nameof(GetProcessors))]
+        public void MapperWorks(ISieveProcessor processor)
         {
             var model = new SieveModel
             {
                 Filters = "shortname@=A",
             };
 
-            var result = _processor.Apply(model, _posts);
+            var result = processor.Apply(model, _posts);
 
             Assert.Equal("A", result.First().ThisHasNoAttributeButIsAccessible);
 
             Assert.True(result.Count() == 1);
         }
 
-        [Fact]
-        public void MapperSortOnlyWorks()
+        [Theory]
+        [MemberData(nameof(GetProcessors))]
+        public void MapperSortOnlyWorks(ISieveProcessor processor)
         {
             var model = new SieveModel
             {
@@ -69,9 +91,9 @@ namespace SieveUnitTests
                 Sorts = "OnlySortableViaFluentApi"
             };
 
-            var result = _processor.Apply(model, _posts, applyFiltering: false, applyPagination: false);
+            var result = processor.Apply(model, _posts, applyFiltering: false, applyPagination: false);
 
-            Assert.Throws<SieveMethodNotFoundException>(() => _processor.Apply(model, _posts));
+            Assert.Throws<SieveMethodNotFoundException>(() => processor.Apply(model, _posts));
 
             Assert.Equal(3, result.First().Id);
 
